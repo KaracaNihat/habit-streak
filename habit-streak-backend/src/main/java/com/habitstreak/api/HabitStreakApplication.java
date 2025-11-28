da5f2a75a85394f71c4837aa6ca23aa458d2a375
KaracaNihat <nihatk.prg@gmail.com>
2025-11-28 17:31:26 +0100

    Initial commit: backend and frontend setup

---
+++ habit-streak-backend/src/main/java/com/habitstreak/api/HabitStreakApplication.java
@@ -0,0 +1,13 @@
+package com.habitstreak.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitStreakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitStreakApplication.class, args);
+	}
+
+}
