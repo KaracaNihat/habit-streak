6210cf0dba5106178dcde03cc209f3eb88259ea9
KaracaNihat <nihatk.prg@gmail.com>
2025-11-29 22:13:21 +0100

    feat: apply formatting

--- habit-streak-backend/src/main/java/com/habitstreak/api/model/Habit.java
+++ habit-streak-backend/src/main/java/com/habitstreak/api/model/Habit.java
@@ -1,23 +1,21 @@
 package com.habitstreak.api.model;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
 @Document
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Habit {
-    @Id
-    private String id;
-    private String name;
-    private Integer targetPerWeek;
-    private List<String> completedDays;
-    private String createdAt;
-    private Integer streak;
+  @Id private String id;
+  private String name;
+  private Integer targetPerWeek;
+  private List<String> completedDays;
+  private String createdAt;
+  private Integer streak;
 }
