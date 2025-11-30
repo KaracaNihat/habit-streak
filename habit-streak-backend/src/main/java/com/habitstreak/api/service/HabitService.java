003c955fb788852ed00a0c84257b9a41d1cd2312
KaracaNihat <nihatk.prg@gmail.com>
2025-11-30 02:51:52 +0100

    feat: formatting

--- habit-streak-backend/src/main/java/com/habitstreak/api/service/HabitService.java
+++ habit-streak-backend/src/main/java/com/habitstreak/api/service/HabitService.java
@@ -4,7 +4,6 @@ import com.habitstreak.api.model.Habit;
 import com.habitstreak.api.repository.HabitRepository;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
