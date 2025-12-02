7645eb1aec13e091932e3170e0b8937620a8350d
KaracaNihat <nihatk.prg@gmail.com>
2025-12-02 21:47:42 +0100

    feat: create request and response dto

---
+++ habit-streak-backend/src/main/java/com/habitstreak/api/dto/HabitRequestDTO.java
@@ -0,0 +1,11 @@
+package com.habitstreak.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HabitRequestDTO {
+    private String name;
+    private Integer targetPerWeek;
+}
