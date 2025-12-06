87d840277c2da88dd657ea137b2d8394d89f9ef8
KaracaNihat <nihatk.prg@gmail.com>
2025-12-07 00:24:11 +0100

    feat: add property source

--- habit-streak-backend/src/test/java/com/habitstreak/api/integration/HabitRepositoryIntegrationTest.java
+++ habit-streak-backend/src/test/java/com/habitstreak/api/integration/HabitRepositoryIntegrationTest.java
@@ -11,11 +11,18 @@ import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@DataMongoTest
 @ActiveProfiles("test")
+@TestPropertySource(
+    properties = {
+      "spring.data.mongodb.port=0",
+      "spring.data.mongodb.database=testdb",
+      "spring.mongodb.embedded.version=4.11.0"
+    })
 public class HabitRepositoryIntegrationTest {
   @Autowired HabitRepository habitRepository;
   private Habit habit;
