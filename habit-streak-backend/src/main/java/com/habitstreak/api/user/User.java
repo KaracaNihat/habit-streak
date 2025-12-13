8f14dd8280ba641d0dd6b4fd6110051d9663ac8b
KaracaNihat <nihatk.prg@gmail.com>
2025-12-13 19:43:54 +0100

    feat: add user model and repository

---
+++ habit-streak-backend/src/main/java/com/habitstreak/api/user/User.java
@@ -0,0 +1,19 @@
+package com.habitstreak.api.user;
+
+import java.util.UUID;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "users")
+public class User {
+
+  @Id private UUID id = UUID.randomUUID();
+
+  @Indexed(unique = true)
+  private String email;
+
+  private String password;
+}
