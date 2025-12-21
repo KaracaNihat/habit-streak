16cc65e5dcc438dd432bcccb3b5161aaf3fed4b4
KaracaNihat <nihatk.prg@gmail.com>
2025-12-21 13:43:37 +0100

    feat: add routing to dashboard after login

--- habit-streak-frontend/src/Login.tsx
+++ habit-streak-frontend/src/Login.tsx
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import {loginUser} from "./authApi";
-import {Box, Button, Container, Stack, TextField} from "@mui/material";
+import {Button, Container, Stack, TextField} from "@mui/material";
 import SendIcon from '@mui/icons-material/Send';
+import {useNavigate} from "react-router-dom";
 
 const Login: React.FC = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -14,6 +16,7 @@ const Login: React.FC = () => {
             if (res.token) {
                 localStorage.setItem("jwt", res.token);
                 alert("Logged in!");
+                navigate("/dashboard");
             } else {
                 alert("Login failed: no token received");
             }
@@ -24,33 +27,36 @@ const Login: React.FC = () => {
     };
 
     return (
-        <Container maxWidth="sm">
-            <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh" sx={{ bgcolor: '#cfe8fc'}} >
-                <form onSubmit={handleSubmit}>
-                    <Stack spacing={2}>
-                        <TextField
-                            id="outlined-basic"
-                            label="Email"
-                            variant="outlined"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Email"
-                            required/>
-                        <TextField
-                            id="outlined-basic"
-                            label="Password"
-                            type="password"
-                            value={password}
-                            variant="outlined"
-                            onChange={(e) => setPassword(e.target.value)}
-                            placeholder="Email"
-                            required/>
-                        <Button type="submit" variant="contained" endIcon={<SendIcon/>}>
-                            Login
-                        </Button>
-                    </Stack>
-                </form>
-            </Box>
+        <Container maxWidth="sm" sx={{
+            minHeight: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+        }}>
+            <form onSubmit={handleSubmit}>
+                <Stack spacing={2}>
+                    <TextField
+                        id="outlined-basic"
+                        label="Email"
+                        variant="outlined"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email"
+                        required/>
+                    <TextField
+                        id="outlined-basic"
+                        label="Password"
+                        type="password"
+                        value={password}
+                        variant="outlined"
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Email"
+                        required/>
+                    <Button type="submit" variant="contained" endIcon={<SendIcon/>}>
+                        Login
+                    </Button>
+                </Stack>
+            </form>
         </Container>
     );
 };
--- habit-streak-frontend/src/index.tsx
+++ habit-streak-frontend/src/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import Login from "./Login";
+import Dashboard from "./Dashboard";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Login></Login>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route
+                    path="/dashboard"
+                    element={
+                        <PrivateRoute>
+                            <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
